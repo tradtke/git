414902dc16ed2d3ef6a3cada43f9fd775aca83a6
Tim Radtke <tjradtke@mtu.edu>
2016-05-17 09:49:11 -0400

    Simple Twitter App

---
+++ auth.py
@@ -0,0 +1,4 @@
+consumer_key        = "eNncElhF2wQm2zLdsr9v0L0LO"
+consumer_secret     = "P7tJ8sLQwwIH6aZTUJ8FL9HpjCUmNRm9OnQLiS20mwOdeENm"
+access_token        = "732302159849476096-L8v4c1CeLv91GKdjRas41klsA7gVzZi"
+access_token_secret = "dn8X1CJufdVtx1bFkjpDvqvM8BosSPru9quhbTsdaXSc"
